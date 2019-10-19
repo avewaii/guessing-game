d922224a5ca66f7bb011184d635da9b92aa173bd
avewaii <ewashadevsk@gmail.com>
2019-10-19 15:54:28 +0300

    feat: guess number

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round(this.min + (this.max - this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
